222a12c3c206ef377b90f839f3615a65acbc3866
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2022-03-02 13:03:54 -0500

    first commit

---
+++ pages/estudiantes.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const estudiantes = () => {
+  return (
+    <div>estudiantes</div>
+  )
+}
+
+export default estudiantes
---
+++ pages/profesores.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const profesores = () => {
+  return (
+    <div>profesores</div>
+  )
+}
+
+export default profesores
